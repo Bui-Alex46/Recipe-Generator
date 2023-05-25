2f1ca485f255499f242b2c62491bb2487a8bd8fc
jainharshul <harshuljainwork@gmail.com>
2023-05-25 14:50:23 -0700

    update and import pandas
    get csv file in code

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import pandas as pd
 import random
 
 app = Flask(__name__)
@@ -7,3 +8,8 @@ app = Flask(__name__)
 @app.route('/')
 def welcome():
     return render_template('index.html')
+
+print('python file working')
+data = pd.read_csv('/Users/harshuljain/Desktop/dataset')
+headers = list(data.columns.values)
+print(headers)
